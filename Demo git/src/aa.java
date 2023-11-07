79651e15294c075e8a6b16c957cf82660d9871c7
bhlong1710 <builong1710@gmail.com>
2023-11-07 14:24:24 +0700

    123

---
+++ Demo git/src/aa.java
@@ -0,0 +1,17 @@
+
+import java.awt.print.Printable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author builo
+ */
+public class aa {
+   public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
